95ef28a9e32117d5df20ff58893c4a9bedc510d5
how200284 <how200284@gmail.com>
2022-09-07 00:31:35 +0800

    feat: add relations between user and todo models

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
 
+const db = require('./models')
+const Todo = db.Todo
+const User = db.User
+
 const app = express()
 const PORT = 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
@@ -27,7 +31,9 @@ app.get('/users/register', (req, res) => {
 })
 
 app.post('/users/register', (req, res) => {
-  res.send('register')
+  const { name, email, password } = req.body
+  User.create({ name, email, password })
+    .then(user => res.redirect('/'))
 })
 
 app.get('/users/logout', (req, res) => {
