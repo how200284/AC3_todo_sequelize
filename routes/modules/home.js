47080bf18630a651d92cdf0fcdb7020718095b97
how200284 <how200284@gmail.com>
2022-09-09 00:19:27 +0800

    feat: add relations between User and Todo models to ensure each user get their todos only

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,14 +3,22 @@ const router = express.Router()
 
 const db = require('../../models')
 const Todo = db.Todo
+const User = db.User
 
-router.get('/', (req, res) => {
-  return Todo.findAll({
-    raw: true,
-    nest: true
-  })
-    .then(todos => res.render('index', { todos }))
-    .catch(error => res.status(422).json(error))
+router.get('/', async (req, res) => {
+  try {
+    const UserId = req.user.id
+    const user = await User.findByPk(UserId)
+    if (!user) throw new Error('user not found')  // throw new Error will terminate the execution and transfer the error object to catch.
+    const todos = await Todo.findAll({
+      raw: true,
+      nest: true,
+      where: { UserId }  // To make sure the home page shows todos which belong to this user only.
+    })
+    res.render('index', { todos })
+  } catch(err) {
+    res.status(402).json(error)
+  }
 })
 
 module.exports = router
