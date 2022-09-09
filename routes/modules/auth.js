797e942b0567bc79a087bfd749fcf37bdc4c90f3
how200284 <how200284@gmail.com>
2022-09-09 16:40:38 +0800

    feat: refactor views

--- routes/modules/auth.js
+++ routes/modules/auth.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+const passport = require('passport')
+
 const db = require('../../models')
 
 router.get('/facebook', passport.authenticate('facebook', {
